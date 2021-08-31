2888abde48aeeb43c7e6bc8b96462e0a7444c3bb
Gerardo Leal <gleal@atlassian.com>
2021-08-31 22:02:55 +1000

    auth ready

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '1vojm3n9vh'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-acsv7vxg.us.auth0.com', // Auth0 domain
+  clientId: 'zIv5EEMQVL1RkF8DIlgC2yHyflZ40TXT', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
