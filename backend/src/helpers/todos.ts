a09e3460ff672ff80d0ad97653ad83ba4eaef8a7
Gerardo Leal <gleal@atlassian.com>
2021-09-01 00:31:50 +1000

    get all todos for user ready

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,33 @@
+import * as AWS from "aws-sdk";
+// import * as uuid from "uuid";
+// import * as createError from "http-errors";
+
 // import { TodosAccess } from './todosAcess'
 // import { AttachmentUtils } from './attachmentUtils';
-// import { TodoItem } from '../models/TodoItem'
-// import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-// import { createLogger } from '../utils/logger'
-// import * as uuid from 'uuid'
-// import * as createError from 'http-errors'
+import { TodoItem } from "../models/TodoItem";
+// import { CreateTodoRequest } from "../requests/CreateTodoRequest";
+// import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
+import { createLogger } from "../utils/logger";
 
-// TODO: Implement businessLogic
+const todosTable = process.env.TODOS_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
+const logger = createLogger("todos");
+
+export const getTodosForUser = async (userId: string): Promise<TodoItem[]> => {
+  logger.info(`Getting TODOs for user: ${userId}`);
+
+  const result = await docClient
+    .query({
+      TableName: todosTable,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+      ScanIndexForward: false,
+    })
+    .promise();
+
+  logger.info(`Got TODOs for user ${userId}`, result);
+
+  return result.Items as TodoItem[];
+};
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,20 +4,22 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import * as middy from "middy";
 import { cors } from "middy/middlewares";
 
-// import { getTodosForUser as getTodosForUser } from "../../businessLogic/todos";
-// import { getUserId } from "../utils";
+import { getTodosForUser } from "../../helpers/todos";
+import { getUserId } from "../utils";
 
-// TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    // const todos = "...";
-
-    console.log(event);
+    const userId = getUserId(event);
+    const todos = await getTodosForUser(userId);
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "hello world" }),
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        items: todos,
+      }),
     };
   }
 );
